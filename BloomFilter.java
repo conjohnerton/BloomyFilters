1279d235667ff6c4887c463f62414af5e23d25ff
johnnycheadshot <conjohnerton@gmail.com>
2019-06-09 22:07:51 -0400

    finished contains and add functions

--- BloomFilter.java
+++ BloomFilter.java
@@ -2,21 +2,34 @@
 
 import java.util.*;
 
-public class BloomFilter.<T>
+public class BloomFilter<T>
 {
     private boolean[][] filters;
-    public int[] primes = {11, 13, 17, 19, 23, 29, 31, 37, 41, 43};
+    public int[] primes = {11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53};
 
-    public void add(T key)
+    public boolean contains(T key)
     {
+        // HashIt!
         int val = key.hashCode();
 
-        for (int i = 0; i < filters.length; i++)
+        // wrap this bad boy back around to 0 if we have integer overflow
+        if (val < 0)
         {
-            int index = val % filters[i].length;
-            filters[i][index];
+            val -= Integer.MIN_VALUE;
         }
 
+        // check each filter to see if the key is present in each of them
+        for (int i = 0; i < this.filters.length; i++)
+        {
+            int index = val % this.filters[i].length;
+
+            if (this.filters[i][index] == false)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public static void main(String[] args)
@@ -28,17 +41,33 @@ public class BloomFilter.<T>
         }
 
 
-        BloomFilter bloomy = new BloomFilter(Integer.parseInt(args[0]));
-        System.out.println("hello".hashCode());
+        BloomFilter<String> bloomy = new BloomFilter<>(Integer.parseInt(args[0]));
+        bloomy.add("hello");
     }
 
     public BloomFilter(int numFilters)
     {
-        this.filters = new boolean[amountOfFilters][];
+        this.filters = new boolean[numFilters][];
 
-        for (int i = 0; i < amountOfFilters; i++)
+        for (int i = 0; i < numFilters; i++)
         {
             this.filters[i] = new boolean[primes[i]];
+        }
+    }
+
+    public void add(T key)
+    {
+        int val = key.hashCode();
+
+        if (val < 0)
+        {
+            val -= Integer.MIN_VALUE;
+        }
+
+        for (int i = 0; i < this.filters.length; i++)
+        {
+            int index = val % this.filters[i].length;
+            this.filters[i][index] = true;
         }
     }
 }
